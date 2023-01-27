1b4cccb777065441ee31fd6fa5ef51bcb3276c2c
CumMblLLl <nobbiland@gmail.com>
2023-01-27 17:20:42 +0200

    fix

--- src/components/FormField/TextArea.tsx
+++ src/components/FormField/TextArea.tsx
@@ -2,13 +2,17 @@ import React, { FC, KeyboardEvent, useState } from 'react';
 import { useField } from 'formik';
 import { ReactComponent as SendMessageSVG } from '../../assets/message.svg';
 import { ReactComponent as SmileSVG } from '../../assets/smile.svg';
-import EmojiPicker, { EmojiClickData } from 'emoji-picker-react';
+import EmojiPicker, {
+  EmojiClickData,
+  EmojiStyle,
+  Theme,
+} from 'emoji-picker-react';
 
 interface Props {
   name: string;
   placeholder?: string;
   className?: string;
-  sendMessage: () => void;
+  sendMessage: VoidFunction;
   sendMessageEnter: (e: KeyboardEvent<HTMLTextAreaElement>) => void;
 }
 
@@ -25,31 +29,34 @@ const TextArea: FC<Props> = ({
 
   const selectEmoji = (emojiData: EmojiClickData) => setValue(emojiData.emoji);
 
+  const toggleClickEmoji = () => setEmojiBar((prev) => !prev);
+
   return (
-    <div className='h-[40px] relative'>
+    <div className='h-10 relative'>
       {emojiBar && (
         <div className='absolute bottom-12'>
           <EmojiPicker
+            theme={Theme.DARK}
             onEmojiClick={selectEmoji}
             autoFocusSearch={false}
-            searchDisabled={true}
-            lazyLoadEmojis={true}
+            searchDisabled
+            lazyLoadEmojis
+            emojiStyle={EmojiStyle.NATIVE}
           />
         </div>
       )}
-
       <div className='w-full h-full flex bg-white rounded justify-center items-center'>
         <div className='w-8 cursor-pointer mx-2'>
-          <SmileSVG onClick={() => setEmojiBar((prev) => !prev)} />
+          <SmileSVG onClick={toggleClickEmoji} />
         </div>
         <textarea
+          className='w-full h-full resize-none text-black px-3 py-2 rounded focus:outline-0'
           value={value}
           placeholder={placeholder}
-          className='w-full h-full resize-none text-black px-3 py-2 rounded focus:outline-0'
           onKeyDown={sendMessageEnter}
           {...field}
         />
-        <span className='h-full w-[2px] bg-black' />
+        <span className='h-full w-0.5 bg-black' />
         <div
           className='h-full flex items-center cursor-pointer px-3'
           onClick={sendMessage}
