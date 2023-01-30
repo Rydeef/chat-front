bb0ad37327a78f99e9a1a923f2f6d7c3809f91d6
CumMblLLl <nobbiland@gmail.com>
2023-01-30 17:21:44 +0200

    create new drop menu

--- src/components/DropMenu/template/UserMenu.tsx
+++ src/components/DropMenu/template/UserMenu.tsx
@@ -1,25 +1,17 @@
-import React, { useState } from 'react';
-import cn from 'classnames';
+import React from 'react';
 import { useAppDispatch } from '../../../app/hooks';
 import { logOutUserAction } from '../../../app/modules/auth/actions';
 import { ReactComponent as SettingsSvg } from '../../../assets/settings.svg';
 import { ReactComponent as ExitSvg } from '../../../assets/exit.svg';
-import { ReactComponent as SunSvg } from '../../../assets/sun.svg';
 import { ReactComponent as MoonSvg } from '../../../assets/moon.svg';
 
-type ThemeType = 'light' | 'dark';
-
 const STYLE_MENU_ITEM =
-  'flex fill-[#C6C5C5] items-center px-4 py-2 rounded-full cursor-pointer duration-100 hover:bg-gray-2 hover:text-white hover:fill-[#D9D9D9] select-none';
+  'flex fill-[#C6C5C5]/80 select-none mx-2 items-center p-2 rounded-md cursor-pointer duration-100 hover:bg-gray-2 hover:text-white hover:fill-[#C6C5C5]/100 ';
 
 const UserMenu = () => {
   const userName = localStorage.getItem('userName');
 
-  const [currentTheme, setCurrentTheme] = useState<string>('light');
-
   const dispatch = useAppDispatch();
-
-  const switchTheme = (theme: ThemeType) => () => setCurrentTheme(theme);
 
   const logOut = () => {
     dispatch(logOutUserAction());
@@ -27,40 +19,15 @@ const UserMenu = () => {
 
   return (
     <div className='absolute top-0 right-0 z-10'>
-      <div className='bg-[#232325] w-[220px] rounded p-2 grid gap-3 text-[#C6C5C5]'>
-        <div className='text-2xl text-center w-full text-white'>{userName}</div>
-        <div className='border-y-2 border-white dorder-white py-2 grid gap-3'>
-          <div className='flex w-full text-black select-none'>
-            <div
-              className={cn(
-                'flex w-1/2 py-4 bg-white cursor-pointer duration-200 pl-2 rounded-l-lg',
-                {
-                  'bg-[#3980C1] text-white fill-white ':
-                    currentTheme === 'light',
-                }
-              )}
-              onClick={switchTheme('light')}
-            >
-              <SunSvg className='w-6 shrink-0' />
-              <div className='text-center w-full'>light</div>
-            </div>
-            <div
-              className={cn(
-                'flex w-1/2 bg-white py-4 pr-2 rounded-r-lg cursor-pointer duration-200',
-                {
-                  'bg-[#3980C1] text-white fill-white ':
-                    currentTheme === 'dark',
-                }
-              )}
-              onClick={switchTheme('dark')}
-            >
-              <div className='text-center w-full'>dark</div>
-              <MoonSvg className='w-6 shrink-0' />
-            </div>
-          </div>
+      <div className='grid gap-3 bg-[#232325] w-[220px] py-3'>
+        <div className='flex justify-between mx-5'>
+          <span className='text-[24px]'>{userName}</span>
+          <MoonSvg className='w-6 shrink-0 fill-white' />
+        </div>
+        <div className='py-2 border-y-2 border-white'>
           <div className={STYLE_MENU_ITEM}>
             <SettingsSvg className='shrink-0' />
-            <div className='text-xl text-center w-full'>Settings</div>
+            <div className='text-xl pl-5 w-full'>Settings</div>
           </div>
         </div>
         <div onClick={logOut} className={STYLE_MENU_ITEM}>
