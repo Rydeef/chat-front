4fff857a97add9edddcc96ba951787aab33d339c
CumMblLLl <nobbiland@gmail.com>
2023-02-08 16:00:12 +0200

    change fill svg

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as NotFoundSVG } from 'assets/404.svg';
 const NotFound = () => {
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center gap-3'>
-      <NotFoundSVG className='w-[80px] fill-white' />
+      <NotFoundSVG className='w-20 fill-black dark:fill-white' />
       <span className='text-2xl font-bold'>Oooooooops, page not found</span>
     </div>
   );
