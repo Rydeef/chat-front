52a030d29a0663a2d5ba4c4c7dcd1dfe84815ace
CumMblLLl <nobbiland@gmail.com>
2023-02-08 15:22:36 +0200

    change height

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import ChatLink from '../ChatLink/ChatLink';
 const Sidebar = () => {
   return (
     <div className='w-105 h-200 flex flex-col duration-300 bg-slate-400 dark:bg-primary'>
-      <div className='w-full h-[78px] p-5 flex items-center tracking-widest text-white text-lg bg-slate-500 dark:bg-gray-1'>
+      <div className='w-full h-20 p-5 flex items-center tracking-widest text-white text-lg bg-slate-500 dark:bg-gray-1'>
         Chats
       </div>
       <div className='overflow-auto px-3'>
